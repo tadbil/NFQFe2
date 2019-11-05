47d36c6a7124201e7044faf74012633ba5072d6f
tadbil <biliunast@gmail.com>
2019-11-05 10:59:48 +0200

    Genres added

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,54 @@ import axios from 'axios';
 import {endpoints, getImageUrl} from '../config';
 class  App extends React.Component{
 
-
     constructor() {
         super();
 
         this.state = {
             list: [],
+            genres: [],
         };
     }
 
+
+
+    genreSelect = id => {
+        //console.log(this.state.genres().toString());
+        axios.get(endpoints.genreMovies(id)).then(data => {
+            this.setState({
+                list: data.data.results
+            });
+        });
+        this.setState({
+            showPopular: false
+        })
+    };
+
+    popularSelect = () => {
+        axios.get(endpoints.mostPopularMovies())
+            .then((data) => {
+                this.setState({
+                    list: data.data.results,
+                })
+            });
+        this.setState({
+            showPopular: true
+        })
+    }
+
     componentDidMount() {
-        axios
-            .get(endpoints.mostPopularMovies())
+        axios.get(endpoints.genres()).then(data => {
+            this.setState({
+                genres: data.data.genres
+            });
+        });
+
+        axios.get(endpoints.mostPopularMovies())
             .then((data) => {
                 this.setState({
                     list: data.data.results,
                 });
             });
-
     }
 
 
@@ -29,8 +59,27 @@ class  App extends React.Component{
 
         return (
             <div>
+                <nav className='navbar bg-primary'>
+                    <button
+                        type='button'
+                        className='link-button'
+                        onClick={() => this.popularSelect()}
+                    >
+                        Most Popular
+                    </button>
+                    {this.state.genres.map((genre) => (
+                        <button
+                            key={genre.id}
+                            type='button'
+                            className='link-button'
+                            onClick={() => this.genreSelect(genre.id)}
+                        >
+                            {genre.name}
+                        </button>
+                    ))}
+                </nav>
 
-              {this.state.list.map((card) => (
+                {this.state.list.map((card) => (
                   <Card
                       key={card.id}
                       title={card.original_title}
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,4 @@
 import React from 'react';
-// const background = 'background-image: url(\'https://s9.limitedrun.com/images/1101066/8BP079_front_1400x1400.jpg\')';
-
-
 
 class Card extends React.Component {
     constructor(props) {
@@ -29,31 +26,36 @@ class Card extends React.Component {
                         backgroundImage: `url('${backgroundImage}')`
                     }}
                 />
-            
+
                 <div className="card__title">
                     {title}
                 </div>
-            
+
                 <div className="card__like">
                     <i className="fa fa-heart-o"/>
                 </div>
-            
+
                 <div className="card__subtitle">
                     <span>{data}</span>
                     <span>{voteAverage} ({voteCount} votes)</span>
                 </div>
                 <div className="card-info">
                     <div className="card-info__header">SUMMARY</div>
-                    <button className="card-info__hideButton" onClick={this.toogleDescription}>Toogle Description</button>
+                    <button
+                        className="card-info__hideButton"
+                        onClick={this.toogleDescription}
+                    >
+                        Toogle Description
+                    </button>
                     <div className="card-info__description">
-                        {this.state.isDescription?description:''}
+                        {this.state.isDescription ? description : ''}
                     </div>
                 </div>
             </div>
 
         )
-        
+
     }
 }
 
-export  default  Card;
+export default Card;
